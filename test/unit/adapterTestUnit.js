e804c794c47f1050debb28f7848c9285a7ff0097
Michael Evenchick <michael.evenchick@itential.com>
2025-10-01 10:42:17 -0400

    made a commit to the project 13290106.

--- test/unit/adapterTestUnit.js
+++ test/unit/adapterTestUnit.js
@@ -280,7 +280,7 @@ describe('[unit] Tufin_securetrack Adapter Test', () => {
           assert.notEqual(null, packageDotJson.dependencies);
           assert.notEqual('', packageDotJson.dependencies);
           assert.equal('8.17.1', packageDotJson.dependencies.ajv);
-          assert.equal('1.9.0', packageDotJson.dependencies.axios);
+          assert.equal('1.12.2', packageDotJson.dependencies.axios);
           assert.equal('11.1.0', packageDotJson.dependencies.commander);
           assert.equal('11.3.0', packageDotJson.dependencies['fs-extra']);
           assert.equal('10.8.2', packageDotJson.dependencies.mocha);
