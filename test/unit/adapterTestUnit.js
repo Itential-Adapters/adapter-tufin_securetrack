e7db34f09d0aeb520f6709b6e6f862bbc183dea3
Michael Evenchick <michael.evenchick@itential.com>
2026-03-04 09:54:48 -0500

    patch/auto-remediate/Updating axios to 1.13.6 to get new package-lock

--- test/unit/adapterTestUnit.js
+++ test/unit/adapterTestUnit.js
@@ -280,7 +280,7 @@ describe('[unit] Tufin_securetrack Adapter Test', () => {
           assert.notEqual(null, packageDotJson.dependencies);
           assert.notEqual('', packageDotJson.dependencies);
           assert.equal('8.18.0', packageDotJson.dependencies.ajv);
-          assert.equal('1.13.5', packageDotJson.dependencies.axios);
+          assert.equal('1.13.6', packageDotJson.dependencies.axios);
           assert.equal('11.1.0', packageDotJson.dependencies.commander);
           assert.equal('11.3.3', packageDotJson.dependencies['fs-extra']);
           assert.equal('11.3.0', packageDotJson.dependencies.mocha);
